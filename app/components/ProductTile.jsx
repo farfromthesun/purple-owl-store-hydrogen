cafc0d9138737dbbf2353f02277c0a7299ea18da
Daniel Ochenkowski <daniel.ochenkowski@gmail.com>
2025-01-05 00:14:59 +0100

    product tile hover animation

--- app/components/ProductTile.jsx
+++ app/components/ProductTile.jsx
@@ -1,5 +1,7 @@
 import {Link} from '@remix-run/react';
 import {Image, Money} from '@shopify/hydrogen';
+import {useAnimationControls, motion} from 'framer-motion';
+import {useEffect, useState} from 'react';
 
 /**
  * @param {{
@@ -19,6 +21,41 @@ export function ProductTile({
   const isOnSale =
     product.compareAtPriceRange.minVariantPrice.amount > 0 &&
     product.compareAtPriceRange.maxVariantPrice.amount > 0;
+  const controls = useAnimationControls();
+  const MotionImage = motion(Image);
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  useEffect(() => {
+    setIsDesktop(
+      window.matchMedia('(min-width: 1200px)').matches ? true : false,
+    );
+  }, []);
+
+  const handleMouseEnter = () => {
+    if (!isDesktop) return;
+    controls.start({
+      filter: ['blur(4px)', 'blur(0px)'],
+      scale: [1.05, 1.05],
+      transition: {
+        duration: 0.6,
+        times: [0.5, 1],
+        ease: 'easeOut',
+      },
+    });
+  };
+
+  const handleMouseLeave = () => {
+    if (!isDesktop) return;
+    controls.start({
+      filter: ['blur(4px)', 'blur(0px)'],
+      scale: [1, 1],
+      transition: {
+        duration: 0.6,
+        times: [0.5, 1],
+        ease: 'easeOut',
+      },
+    });
+  };
 
   return (
     <Link
@@ -28,16 +65,41 @@ export function ProductTile({
       className="group opacity-0 invisible animate-fade-slide-v-blur-in-longer"
       style={{animationDelay: (index % animationDelayModulo) * 100 + 'ms'}}
       preventScrollReset
+      // onMouseEnter={handleMouseEnter}
+      // onMouseLeave={handleMouseLeave}
+      onMouseEnter={() =>
+        controls.start({
+          filter: ['blur(4px)', 'blur(4px)', 'blur(0px)', 'blur(0px)'],
+          scale: [1.05, 1.05, 1.05, 1.05],
+          transition: {
+            duration: 0.7,
+            times: [0.25, 0.5, 0.75, 1],
+            ease: 'easeOut',
+          },
+        })
+      }
+      onMouseLeave={() =>
+        controls.start({
+          filter: ['blur(4px)', 'blur(4px)', 'blur(0px)', 'blur(0px)'],
+          scale: [1, 1, 1, 1],
+          transition: {
+            duration: 0.7,
+            times: [0.25, 0.5, 0.75, 1],
+            ease: 'easeOut',
+          },
+        })
+      }
     >
       {product.featuredImage && (
         <div className="relative aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-          <Image
+          <MotionImage
+            animate={controls}
             alt={product.featuredImage.altText || product.title}
             data={product.featuredImage}
             aspectRatio="1/1"
             loading={imgLoading || 'eager'}
             sizes="(min-width: 45em) 400px, 50vw"
-            className="h-full w-full object-cover object-center group-hover:opacity-75 transition ease-[ease] duration-300"
+            className="h-full w-full object-cover object-center transition ease-[ease] duration-300"
           />
           {(isSoldOut || isOnSale) && (
             <div className="absolute bottom-2 flex left-2 gap-2">
